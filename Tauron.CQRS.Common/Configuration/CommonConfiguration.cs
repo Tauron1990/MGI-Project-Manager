5384b37c36a1e64606e6cfde5784d7927cd19d21
Alexander Bachmaier <Tauron.ab@gmail.com>
2019-05-28 14:12:25 +0200

    Commit

---
+++ Tauron.CQRS.Common/Configuration/CommonConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Annotations;
+using Tauron.CQRS.Common.Dto.TypeHandling;
+
+namespace Tauron.CQRS.Common.Configuration
+{
+    [PublicAPI]
+    public class CommonConfiguration
+    {
+        private readonly ITypeRegistry _registry = TypeResolver.TypeRegistry;
+        public void RegisterType(string name, Type namedType) => _registry.Register(name, namedType);
+
+
+    }
+}
