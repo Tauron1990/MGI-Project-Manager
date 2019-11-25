6e37e5a0f41feb0d526d0dd493902d4abee21ed6
Alexander Bachmaier <Tauron.ab@gmail.com>
2019-11-25 13:52:19 +0100

    Commit

--- Tauron.Application.Deployment.AutoUpload/ViewModels/BuildCommand/BuildSelectProjectViewModel.cs
+++ Tauron.Application.Deployment.AutoUpload/ViewModels/BuildCommand/BuildSelectProjectViewModel.cs
@@ -1,13 +1,44 @@
-﻿using Scrutor;
+﻿using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Catel.Collections;
+using Scrutor;
+using Tauron.Application.Deployment.AutoUpload.Models.Core;
+using Tauron.Application.Deployment.AutoUpload.Models.Github;
 using Tauron.Application.Deployment.AutoUpload.ViewModels.Operations;
 
 namespace Tauron.Application.Deployment.AutoUpload.ViewModels.BuildCommand
 {
-    public sealed class 
-
     [ServiceDescriptor(typeof(BuildSelectProjectViewModel))]
     public sealed class BuildSelectProjectViewModel : OperationViewModel<BuildOperationContext>
     {
-        
+        private readonly Settings _settings;
+
+        public FastObservableCollection<PotentialProjekt> Projekts { get; } = new FastObservableCollection<PotentialProjekt>();
+
+        public PotentialProjekt? Projekt { get; set; }
+
+        public BuildSelectProjectViewModel(Settings settings) 
+            => _settings = settings;
+
+        protected override Task InitializeAsync()
+        {
+
+            foreach (var repository in _settings.RegistratedRepositories) 
+                Projekts.Add(new PotentialProjekt(repository.ToString(), async () => await NewProjectAction(repository)));
+
+            Projekts.Add(new PotentialProjekt("Neu...", BuildProjectAction));
+
+            return Task.CompletedTask;
+        }
+
+        private async Task NewProjectAction(RegistratedRepository registratedRepository)
+        {
+
+        }
+
+        private async Task BuildProjectAction()
+        {
+
+        }
     }
 }
---
+++ Tauron.Application.Deployment.AutoUpload/ViewModels/BuildCommand/PotentialProjekt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tauron.Application.Deployment.AutoUpload.ViewModels.BuildCommand
+{
+    public sealed class PotentialProjekt
+    {
+        private readonly Func<Task> _action; 
+
+        public string Name { get; }
+
+        public PotentialProjekt(string name, Func<Task> action)
+        {
+            Name = name;
+            _action = action;
+        }
+    }
+}
