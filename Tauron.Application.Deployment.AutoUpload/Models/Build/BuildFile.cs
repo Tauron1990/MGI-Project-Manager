73d344579a6335f0f83244c6be9a92f7baf51652
Alexander Bachmaier <Tauron.ab@gmail.com>
2019-12-31 05:10:41 +0100

    Commit

---
+++ Tauron.Application.Deployment.AutoUpload/Models/Build/BuildFile.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Tauron.Application.Deployment.AutoUpload.Models.Github;
+
+namespace Tauron.Application.Deployment.AutoUpload.Models.Build
+{
+    public class BuildFile
+    {
+        private BuildFile()
+        {
+
+        }
+
+        public static async Task<BuildFile> Read(RegistratedRepository registratedRepository)
+        {
+            var targetPath = Path.Combine(
+                Path.GetDirectoryName(registratedRepository.ProjectName) ?? string.Empty,
+                "build.xml");
+
+            if (!File.Exists(targetPath))
+                return new BuildFile();
+
+            var ele = XElement.Parse(await File.ReadAllTextAsync(targetPath));
+            var file = new BuildFile();
+
+            foreach (var project in ele.Elements("Project"))
+            {
+                var output = project.Element("Output")?.Value ?? string.Empty;
+            }
+
+            return file;
+        }
+    }
+}
