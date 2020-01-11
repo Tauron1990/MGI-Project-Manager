3ac7c124c090cf24e7a4dd37a1d18341ec0a4ce8
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-01-11 11:09:20 +0100

    Commit

--- Tauron.Application.Deployment.AutoUpload/ViewModels/UploadCommand/UploadCreatePackageViewModel.cs
+++ Tauron.Application.Deployment.AutoUpload/ViewModels/UploadCommand/UploadCreatePackageViewModel.cs
@@ -1,13 +1,48 @@
-﻿using Scrutor;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Catel.Services;
+using Scrutor;
 using Tauron.Application.Deployment.AutoUpload.ViewModels.Operations;
+using Tauron.Application.Wpf;
 
 namespace Tauron.Application.Deployment.AutoUpload.ViewModels.UploadCommand
 {
     [ServiceDescriptor(typeof(UploadCreatePackageViewModel))]
-    public class UploadCreatePackageViewModel : OperationViewModel<UploadCommandContext>
+    public class UploadCreatePackageViewModel : OperationViewModel<UploadCommandContext>, IDisposable
     {
-        private bool _cancel;
+        private readonly IMessageService _messageService;
+        private readonly CancellationTokenSource _cancel = new CancellationTokenSource();
 
         public string Console { get; set; } = string.Empty;
+
+        public int CurrentValue { get; set; }
+
+        public UploadCreatePackageViewModel(IMessageService messageService) 
+            => _messageService = messageService;
+
+        [CommandTarget]
+        public void CancelOp() 
+            => _cancel.Cancel();
+
+        [CommandTarget]
+        public bool CanCancelOp() 
+            => !_cancel.IsCancellationRequested;
+
+        protected override Task InitializeAsync()
+        {
+            Task.Run(StartOperation);
+            return base.InitializeAsync();
+        }
+
+        private async void StartOperation()
+        {
+            
+        }
+
+        private void AddConsole(string value) 
+            => Console = $"{Console}{Environment.NewLine}{value}";
+
+        public void Dispose() => _cancel.Dispose();
     }
 }
--- Tauron.Application.Deployment.AutoUpload/Views/UploadCommand/UploadCreatePackageView.xaml.cs
+++ Tauron.Application.Deployment.AutoUpload/Views/UploadCommand/UploadCreatePackageView.xaml.cs
@@ -1,25 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Tauron.Application.Deployment.AutoUpload.ViewModels.UploadCommand;
+using Tauron.Application.Wpf;
 
 namespace Tauron.Application.Deployment.AutoUpload.Views.UploadCommand
 {
     /// <summary>
     /// Interaktionslogik für UploadCreatePackageView.xaml
     /// </summary>
-    []
+    [Control(typeof(UploadCreatePackageViewModel))]
     public partial class UploadCreatePackageView
     {
-        public UploadCreatePackageView()
+        public UploadCreatePackageView(UploadCreatePackageViewModel model)
+            : base(model)
         {
             InitializeComponent();
         }
