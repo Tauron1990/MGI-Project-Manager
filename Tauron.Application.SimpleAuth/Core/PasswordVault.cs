03f9dbff1d4599e7113105775efd35eb46d9c539
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-03-18 08:03:05 +0100

    Commit

--- Tauron.Application.SimpleAuth/Core/PasswordVault.cs
+++ Tauron.Application.SimpleAuth/Core/PasswordVault.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Nito.AsyncEx;
 using Serilog;
 using Serilog.Context;
 using Tauron.Application.Logging;
@@ -16,14 +17,14 @@ namespace Tauron.Application.SimpleAuth.Core
 
 
         private readonly ILogger _logger;
-        private readonly object _lock = new object();
+        private readonly AsyncReaderWriterLock _lock = new AsyncReaderWriterLock();
         private readonly IDisposable _changeToken;
         private readonly PasswordHasher<string> _hasher;
 
         private SimplAuthSettings _settings;
 
         private IAppOptions _appOptions;
-        private IOption _password = OptionImpl.EmptyOption;
+        private IOption? _password;
 
 
         // ReSharper disable once SuggestBaseTypeForParameter
@@ -35,14 +36,17 @@ namespace Tauron.Application.SimpleAuth.Core
             string NameGetter() => $"PasswodVault-{_settings.AppName}";
             _appOptions = optionsStore.GetAppOptions(NameGetter());
 
-            _changeToken = settings.OnChange( parm =>
+            _changeToken = settings.OnChange(parm =>
             {
-                _logger.Information("Simple Auth Settings Changed: {Config}", parm.AppName);
+                _logger.Information("Simple Auth Settings Changed: {SimpleAuthConfig}", parm.AppName);
 
                 _settings = parm;
 
-                lock (_lock)
-                    _appOptions = optionsStore.GetAppOptions(NameGetter());
+                using var l = _lock.WriterLock();
+
+                _appOptions = optionsStore.GetAppOptions(NameGetter());
+                _password = null;
+
             });
         }
 
@@ -51,14 +55,14 @@ namespace Tauron.Application.SimpleAuth.Core
 
         public async Task<bool> CheckPassword(string pass)
         {
-            using var opt = LogContext.PushProperty("Config", _settings.AppName);
+            using var opt = LogContext.PushProperty("SimpleAuthConfig", _settings.AppName);
 
             _logger.Information("Try Check Password");
 
             try
             {
                 var opass = HashPassword(pass);
-                var checkPass = _password.Value;
+                var checkPass = await TryGetPassword();
                 if (string.IsNullOrWhiteSpace(checkPass))
                     checkPass = HashPassword(_settings.BaseAdminPass);
 
@@ -72,9 +76,46 @@ namespace Tauron.Application.SimpleAuth.Core
             return false;
         }
 
-        public Task<bool> SetPassword(string pass)
+        public async Task<bool> SetPassword(string pass)
         {
+            using var opt = LogContext.PushProperty("SimpleAuthConfig", _settings.AppName);
 
+            var newPass = HashPassword(pass);
+            var oldPass = await TryGetPassword();
+            if (newPass == oldPass)
+            {
+                _logger.Information("User Try to Set Same Password");
+
+                return false;
+            }
+
+            if (_password == null)
+            {
+                _logger.Warning("Password Option not Found");
+                return false;
+            }
+
+            await _password.SetValueAsync(newPass);
+            _logger.Information("Password in Simple Auth Changed");
+
+            return true;
+        }
+
+        private async Task<string> TryGetPassword()
+        {
+            using (await _lock.ReaderLockAsync())
+            {
+                if (_password != null)
+                    return _password.Value;
+
+                using var opt = LogContext.PushProperty("SimpleAuthConfig", _settings.AppName);
+
+                _logger.Information("Try acquire password option");
+
+                _password = await _appOptions.GetOptionAsync(PasswortName);
+
+                return _password.Value;
+            }
         }
 
         private string HashPassword(string pass) 
