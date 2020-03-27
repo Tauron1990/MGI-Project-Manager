1628231170e32fc8322a5a0e964442e2a5b28ce5
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-03-27 06:13:49 +0100

    Commit

---
+++ Tauron.Application.SimpleAuth.Tests/Core/SimpleAuthenticationPostConfigureOptionsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Tauron.Application.SimpleAuth.Core;
+using Xunit;
+
+namespace Tauron.Application.SimpleAuth.Tests.Core
+{
+    public sealed class SimpleAuthenticationPostConfigureOptionsTest
+    {
+        [Fact]
+        public void PostConfigureTest()
+        {
+            const string testName = "Test";
+
+            var options = new SimplAuthSettings();
+            var autOptions = new SimpleAuthenticationOptions();
+
+            var test = new SimpleAuthenticationPostConfigureOptions(new OptionsWrapper<SimplAuthSettings>(options));
+
+
+            //Valid 
+            autOptions.Realm = "Test";
+            options.AppName = "Test";
+            autOptions.TokenTimeout = TimeSpan.FromDays(1);
+
+            test.PostConfigure(testName, autOptions);
+
+            //PatiallyValid
+            autOptions.Realm = "";
+
+            test.PostConfigure(testName, autOptions);
+
+            //Invalid 1
+            autOptions.Realm = "";
+            options.AppName = "";
+
+            Assert.Throws<InvalidOperationException>(() => test.PostConfigure(testName, autOptions));
+
+            //Invalid 2
+            autOptions.Realm = "Test";
+            autOptions.TokenTimeout = TimeSpan.FromMilliseconds(1000);
+
+            Assert.Throws<InvalidOperationException>(() => test.PostConfigure(testName, autOptions));
+        }
+    }
+}
--- Tauron.Application.SimpleAuth/Core/SimpleAuthenticationPostConfigureOptions.cs
+++ Tauron.Application.SimpleAuth/Core/SimpleAuthenticationPostConfigureOptions.cs
@@ -5,12 +5,21 @@ namespace Tauron.Application.SimpleAuth.Core
 {
     public class SimpleAuthenticationPostConfigureOptions : IPostConfigureOptions<SimpleAuthenticationOptions>
     {
+        private readonly IOptions<SimplAuthSettings> _settings;
+
+        public SimpleAuthenticationPostConfigureOptions(IOptions<SimplAuthSettings> settings) 
+            => _settings = settings;
+
         public void PostConfigure(string name, SimpleAuthenticationOptions options)
         {
             if (options.TokenTimeout.TotalSeconds < 60)
                 throw new InvalidOperationException("Token Timeout is to Low");
 
-            if (string.IsNullOrEmpty(options.Realm))
+            if (!string.IsNullOrEmpty(options.Realm)) return;
+
+            options.Realm = _settings.Value.AppName;
+
+            if(string.IsNullOrEmpty(options.Realm))
                 throw new InvalidOperationException("Realm must be provided in options");
         }
     }
