a974f6a4d1d9de9145a7233505be60485fd3c7cd
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-04-21 10:44:37 +0200

    Commit

--- Tauron.Application.ToolUi.SimpleAuth/ClientFactory.cs
+++ Tauron.Application.ToolUi.SimpleAuth/ClientFactory.cs
@@ -19,7 +19,23 @@ namespace Tauron.Application.ToolUi.SimpleAuth
 
         public async Task<TClient> Create(string host)
         {
-            var pass = new 
-        }
+            var pass = new
+                I think you should use the below api in CallInvokerExtensions to forward headers:
+
+            public static CallInvoker Intercept(this CallInvoker invoker, Func<Metadata, Metadata> interceptor)
+
+            You need to create your ForwardingHeaderFunc like this:
+
+        public static Func<Metadata, Metadata> ForwardingHeaderFunc = (Metadata source) =>
+        {
+            foreach (var httpHeader in _forwardHeadersProvider.GetForwardedHeaders())
+            {
+                source.Add(httpHeader.Key, httpHeader.Value);
+            }
+
+            return source;
+        };
+
     }
+}
 }
